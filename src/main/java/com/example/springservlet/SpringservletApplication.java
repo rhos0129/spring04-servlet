f8bced0800d63281dfeea917223b001f5ff6741a
rhos0129 <rhos0129@gmail.com>
2024-08-18 21:26:05 +0900

    S02 프로젝트 생성

---
+++ src/main/java/com/example/springservlet/SpringservletApplication.java
@@ -0,0 +1,13 @@
+package com.example.springservlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringservletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringservletApplication.class, args);
+	}
+
+}
