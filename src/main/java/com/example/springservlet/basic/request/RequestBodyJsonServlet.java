d1f1377a074c356569f3b8783878aa6ad0b49f06
rhos0129 <rhos0129@gmail.com>
2024-08-19 12:22:02 +0900

    S02 HTTP 요청 데이터 - API 메시지 바디 - JSON

---
+++ src/main/java/com/example/springservlet/basic/request/RequestBodyJsonServlet.java
@@ -0,0 +1,38 @@
+package com.example.springservlet.basic.request;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.example.springservlet.basic.HelloData;
+import org.springframework.util.StreamUtils;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "requestBodyJsonServlet", urlPatterns = "/request-body-json")
+public class RequestBodyJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // HTTP 메시지 바디의 데이터 읽기
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        System.out.println("messageBody = " + messageBody);
+
+        // HTTP 메시지 바디의 데이터를 자바 객체로 변환
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+
+        System.out.println("helloData.username = " + helloData.getUsername());
+        System.out.println("helloData.age = " + helloData.getAge());
+
+        response.getWriter().write("ok");
+    }
+}
