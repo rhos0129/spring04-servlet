bc830745836c9203764f914db119ad332ad93a78
rhos0129 <rhos0129@gmail.com>
2024-08-19 12:37:38 +0900

    S02 HTTP 응답 데이터 - API JSON

---
+++ src/main/java/com/example/springservlet/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,32 @@
+package com.example.springservlet.basic.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.example.springservlet.basic.HelloData;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // Content-Type: application/json;charset=utf-8
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+
+        HelloData helloData = new HelloData();
+        helloData.setUsername("kim");
+        helloData.setAge(20);
+
+        String result = objectMapper.writeValueAsString(helloData); // 객체 -> json
+        response.getWriter().write(result);
+    }
+}
