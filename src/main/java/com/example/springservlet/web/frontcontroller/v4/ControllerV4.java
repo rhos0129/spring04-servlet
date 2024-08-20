228d8140108ef96b494e6ca9c7e96747598c5c12
rhos0129 <rhos0129@gmail.com>
2024-08-20 14:50:13 +0900

    S04 단순하고 실용적인 컨트롤러 - v4

---
+++ src/main/java/com/example/springservlet/web/frontcontroller/v4/ControllerV4.java
@@ -0,0 +1,9 @@
+package com.example.springservlet.web.frontcontroller.v4;
+
+import java.util.Map;
+
+public interface ControllerV4 {
+
+    String process(Map<String, String> paramMap, Map<String, Object> model);
+
+}
---
+++ src/main/java/com/example/springservlet/web/frontcontroller/v4/controller/MemberListControllerV4.java
@@ -0,0 +1,23 @@
+package com.example.springservlet.web.frontcontroller.v4.controller;
+
+import com.example.springservlet.domain.member.Member;
+import com.example.springservlet.domain.member.MemberRepository;
+import com.example.springservlet.web.frontcontroller.v4.ControllerV4;
+
+import java.util.List;
+import java.util.Map;
+
+public class MemberListControllerV4 implements ControllerV4 {
+
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public String process(Map<String, String> paramMap, Map<String, Object> model) {
+        List<Member> members = memberRepository.findAll();
+
+        model.put("members", members);
+
+        return "members";
+    }
+
+}
