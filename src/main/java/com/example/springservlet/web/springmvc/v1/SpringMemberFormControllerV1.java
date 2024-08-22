bb3a67594c3307b544c6d35b0cce4ee34d17a3f5
rhos0129 <rhos0129@gmail.com>
2024-08-22 13:35:18 +0900

    S05 스프링 MVC - 시작하기

---
+++ src/main/java/com/example/springservlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -0,0 +1,15 @@
+package com.example.springservlet.web.springmvc.v1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SpringMemberFormControllerV1 {
+
+    @RequestMapping("/springmvc/v1/members/new-form") // RequestMappingHandlerMapping, RequestMappingHandlerAdapter
+    public ModelAndView process() {
+        return new ModelAndView("new-form");
+    }
+
+}
---
+++ src/main/java/com/example/springservlet/web/springmvc/v1/SpringMemberListControllerV1.java
@@ -0,0 +1,26 @@
+package com.example.springservlet.web.springmvc.v1;
+
+import com.example.springservlet.domain.member.Member;
+import com.example.springservlet.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class SpringMemberListControllerV1 {
+
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members") // RequestMappingHandlerMapping, RequestMappingHandlerAdapter
+    public ModelAndView process() {
+
+        List<Member> members = memberRepository.findAll();
+
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", members);
+        return mv;
+    }
+
+}
