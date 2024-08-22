2498613919af33c3bef04a62e89d4744e7bbcc12
rhos0129 <rhos0129@gmail.com>
2024-08-22 13:36:46 +0900

    S05 스프링 MVC - 컨트롤러 통합

---
+++ src/main/java/com/example/springservlet/web/springmvc/v2/SpringMemberControllerV2.java
@@ -0,0 +1,46 @@
+package com.example.springservlet.web.springmvc.v2;
+
+import com.example.springservlet.domain.member.Member;
+import com.example.springservlet.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@Controller
+@RequestMapping("/springmvc/v2/members") // 메서드 레벨의 @RequestMapping와 조합하여 url 중복 제거 가능
+public class SpringMemberControllerV2 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/new-form")
+    public ModelAndView newForm() {
+        return new ModelAndView("new-form");
+    }
+
+    @RequestMapping("/save")
+    public ModelAndView save(HttpServletRequest request, HttpServletResponse response) {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        ModelAndView mv = new ModelAndView("save-result");
+        mv.addObject("member", member);
+        return mv;
+    }
+
+    @RequestMapping
+    public ModelAndView members() {
+        List<Member> members = memberRepository.findAll();
+
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", members);
+        return mv;
+    }
+
+}
